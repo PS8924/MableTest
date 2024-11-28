f62f7608ef3062f40f204b0e7f03a45f90333792
PS8924 <git@stackblitz.com>
2024-11-27 21:21:38 -0800

    Initial commit

---
+++ src/cheapProduct.ts
@@ -0,0 +1,23 @@
+import { Product } from './model/product';
+
+const findCheapestProduct = (products: Product[]): Product => {
+  let minPrice = Infinity;
+  let cheapProduct!: Product;
+
+  products.forEach((product) => {
+    if (product.price < minPrice) {
+      cheapProduct = product;
+      minPrice = product.price;
+    }
+  });
+
+  return cheapProduct;
+};
+
+let listOfProducts = [
+  { id: 1, name: 'Product-1', price: 99, msrp: 99 },
+  { id: 2, name: 'Product-2', price: 50, msrp: 99 },
+  { id: 3, name: 'Product-3', price: 60, msrp: 99 },
+];
+
+console.log('Cheap Product is: ', findCheapestProduct(listOfProducts).name);
