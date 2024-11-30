d6b95066d405a3e11ada25bb82eb557dd48697e3
PS8924 <shah.pankti@northeastern.edu>
2024-11-29 16:27:16 -0800

    Question 4: Max Profit from Stock history

---
+++ src/maxProfitStock.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (const price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else {
+            const profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+    return maxProfit;
+}
+
+console.log(maxProfit([4, 7, 2, 6, 1])); // 4
+console.log(maxProfit([5, 4, 3, 2, 1])); // 0
